c5e1e611dc6ce8479b44b1d186f40043f718bf1d
HibHaqqi <haqqi09@gmail.com>
2025-02-06 14:31:10 +0700

    setup view ejs

--- Index.js
+++ Index.js
@@ -11,7 +11,10 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({
   extended :false
 }));
+app.set('view engine', 'ejs')
+app.set('views', "./pages")
+
 app.use(morgan("dev"));
-app.get("/", (req, res) => res.send("Hello World!"));
-app.use(router);
+
+app.use("/",router);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
